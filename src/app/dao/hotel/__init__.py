c90c040f7c68be9d01db50b57fac22990d54066a
al1enn <wipetmp1404@proton.me>
2025-04-12 16:03:44 +0500

    Add images relationship to HotelDAO for enhanced data retrieval
    - Updated HotelDAO to include the loading of images related to hotels, improving the data structure and retrieval capabilities for hotel information.
    - This change enhances the overall functionality of the hotel data access layer.

--- src/app/dao/hotel/__init__.py
+++ src/app/dao/hotel/__init__.py
@@ -121,6 +121,7 @@ class HotelDAO(BaseDAO):
                 selectinload(cls.model.rule),
                 selectinload(cls.model.hotel_category),
                 selectinload(cls.model.hotel_rating),
+                selectinload(cls.model.images),
             )
             .where(cls.model.id == hotel_id)
         )
