f4e61238b0c0bd489e9d735113fa26f4672e932b
al1enn <wipetmp1404@proton.me>
2025-04-14 12:14:33 +0500

    Add role validation in get_user_by_token_sub function
    - Introduced a check to ensure the token payload contains a user role, raising an UnauthorizedException if the role is not 'user'. This enhances security by validating token types before processing user data.

--- src/app/core/auth/validation.py
+++ src/app/core/auth/validation.py
@@ -86,6 +86,8 @@ async def get_refresh_token_payload(
 
 
 async def get_user_by_token_sub(session: AsyncSession, payload: dict) -> UserBase:
+    if payload.get("role") != "user":
+        raise UnauthorizedException("Invalid token type (user not found)")
     user_id: str | None = payload.get("sub")
     # todo: check token blacklist
     jti = payload.get("jti")
