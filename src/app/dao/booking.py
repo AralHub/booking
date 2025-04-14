1a5b7cdce03ab08b4ca1ff084c4dca1faebc75c0
al1enn <wipetmp1404@proton.me>
2025-04-14 18:45:26 +0500

    Refactor BookingDAO to optimize room type loading strategy
    - Updated the BookingDAO to use joinedload for room_type, enhancing the efficiency of data retrieval for room-related information.
    - This change improves the overall performance of booking data queries by streamlining the loading process.

--- src/app/dao/booking.py
+++ src/app/dao/booking.py
@@ -435,8 +435,7 @@ class BookingDAO(BaseDAO):
                 .selectinload(Room.room_images),
                 selectinload(Booking.booking_rooms)
                 .selectinload(BookedRoom.room)
-                .joinedload(Room.room_type)
-                .load_only(RoomType.name),
+                .joinedload(Room.room_type),
                 joinedload(Booking.user).load_only(
                     User.id,
                     User.first_name,
