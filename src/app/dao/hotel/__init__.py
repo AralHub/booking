2ba23607e3239c23f0fe295aaf5441ecb2c16ec5
al1enn <wipetmp1404@proton.me>
2025-04-15 13:05:45 +0500

    Add city_slug to hotel data response in HotelDAO for enhanced location details

--- src/app/dao/hotel/__init__.py
+++ src/app/dao/hotel/__init__.py
@@ -124,6 +124,7 @@ class HotelDAO(BaseDAO):
                 "min_price": min_price,
                 "guests": min_guests,
                 "city": city_data.get("name"),
+                "city_slug": city_data.get("slug"),
                 "to_city_center": hotel.location.to_city_center,
             }
             # Добавляем рейтинг на верхний уровень
