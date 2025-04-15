c2323157c8638725e38a7e68e8aec287b7025096
al1enn <wipetmp1404@proton.me>
2025-04-15 17:53:44 +0500

    Add user favorites retrieval method in UserFavoriteDAO
    - Implemented a new asynchronous method to fetch user favorites by user ID, enhancing the functionality of the favorites feature.
    - Integrated HotelDAO to retrieve full hotel details for each favorite, improving the data returned to users.

--- src/app/dao/favorites.py
+++ src/app/dao/favorites.py
@@ -1,8 +1,31 @@
-# from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession
 from app.dao import BaseDAO
-
+from app.dao.hotel import HotelDAO
 from app.models.favorites import UserFavorite
+from app.schemas.favorites import UserFavoriteFilter
+from app.schemas.hotel import HotelFilter
 
 
 class UserFavoriteDAO(BaseDAO):
     model = UserFavorite
+
+    @classmethod
+    async def get_user_favorites(
+        cls,
+        session: AsyncSession,
+        user_id: int,
+    ):
+        hotel_ids = await cls.get_all(
+            session=session,
+            filters=UserFavoriteFilter(
+                user_id=user_id,
+            ),
+        )
+        favorites = []
+        for hotel_id in hotel_ids:
+            hotel = await HotelDAO.get_full_hotel_by_id(
+                session=session,
+                hotel_id=hotel_id,
+            )
+            favorites.append(hotel)
+        return favorites
